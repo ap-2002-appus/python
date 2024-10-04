ec5dfe8c08a4859d7f0c70ab697aebddfc1b2f20
ap-2002-appus <ibinsiby4619@gmail.com>
2024-10-04 16:09:27 +0530

    Add files via upload

---
+++ ibin39.py
@@ -0,0 +1,77 @@
+print("Hello \nWorld")
+'''
+if 5 > 2:
+  print("Five is  greater than two")
+if 5 > 2:
+    print("Five is  greater than two")
+if 5 > 2:
+             print("Five is  greater than two")
+
+
+if 5>2:
+    print("Five is greater than two")
+    print("five is greater than two")
+
+
+ x=5
+ y="thomas"
+  print(x)
+  print(y)
+
+
+x = str(3)
+y = int(3)
+z= float(3)
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "appu"
+print(x)
+print(y)
+
+
+a = 4
+A = "Thomas"
+print(a,A)
+
+
+x , y, z = "Oramge" ,"Banana" ,"Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Oramge" ,"Banana" ,"Cherry"
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Oramge"
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = " is"
+z = " awesome"
+print (x + y + z)
+
+
+username = input( "Enter username:")
+print("username is: " + username)
+
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+
+a = "Hello, World!"
+print(a[6])
+'''
+b = "Hello, World!"
+print(b[:5])
